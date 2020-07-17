c1c5c1e36967b27ba1eec2be8d6a738cac6b79bd
Carter Cherry <6109635+cartercherry@users.noreply.github.com>
2020-07-16 20:50:14 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// const URL = "api/ron/quotes";
+const URL = "ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#ron").addEventListener("click", getRon);
+});
+
+function getRon(ev) {
+  ev.preventDefault();
+  console.log("Getting Ron quote...");
+  fetch(URL)
+    .then((res) => resp.json())
+    .then((content) => {
+      let main = document.querySelector("main");
+      main.innerHTML = `<h2>${content[0]}</h2>`;
+    })
+    .catch((err) => console.error);
+}
